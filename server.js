deacf0c2930b80a492cc884998cbeaa4a76bcd59
NIKHILBNJ <nikhilbabureddynj95@gmail.com>
2018-07-03 11:58:54 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+console.log("server.js started...");
+
+
+var express = require('express');
+
+var path = require('path');
+
+var app = express();
+
+const publicPath  = path.join(__dirname,'../public');
+app.use(express.static(publicPath));
+
+// app.get('/',(req,res)=>{
+
+//     res.render('index.html')
+// })
+
+
+app.listen(3000);
